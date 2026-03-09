6127aa441e4ca6910738f5933abd67b700bc4048
cannedfart <jayromeramirez19@gmail.com>
2026-03-09 07:40:05 -0400

    Add files via upload

--- meta/js/spa.js
+++ meta/js/spa.js
@@ -5,10 +5,10 @@ function changeContent(page) {
 		case 'home':
 			contentDiv.innerHTML = `
 				<div class="navi-bar">
-					<h4 href="#top" onclick="changeContent('home'); swapStyleSheet('meta/css/main.css'); select;">Brewtendo</h4>
-					<h4 href="#top" onclick="changeContent('guide'); swapStyleSheet('meta/css/guide.css'); select;">Install Guide</h4>
-					<h4 href="#top" onclick="changeContent('badgearcade'); swapStyleSheet('meta/css/main.css'); select;">Badge Arcade</h4>
-					<h4 href="#top" onclick="changeContent('other'); swapStyleSheet('meta/css/main.css'); select;">Other</h4>
+					<h4 href="#top" onclick="changeContent('home'); swapStyleSheet('meta/css/main.css'); playAudio('decide');">Brewtendo</h4>
+					<h4 href="#top" onclick="changeContent('guide'); swapStyleSheet('meta/css/guide.css'); playAudio('decide');">Install Guide</h4>
+					<h4 href="#top" onclick="changeContent('badgearcade'); swapStyleSheet('meta/css/guide.css'); playAudio('decide');">Badge Arcade</h4>
+					<h4 href="#top" onclick="changeContent('other'); swapStyleSheet('meta/css/main.css'); playAudio('decide');">Other</h4>
 				</div>
             	<!--NOT AI BTW-->
 				<div id="center-div">
@@ -17,7 +17,7 @@ function changeContent(page) {
 					<h3>by cannedfart <img class="pfp" src="https://cdn.discordapp.com/avatars/1106930666333028422/f676c7fde54ed42351e094cac5825324.png" /> & barely nebula <img class="pfp" src="https://cdn.discordapp.com/avatars/1407843801762107535/aa55584f743396e1adcd23d949939096.png"> btw :)</h3>
 					<div class="style-center-div">
 						<h4><i>View the guide to learn how to install Brewtendo on your 3DS!</i></h4>
-						<div id="bottom-pill-button" class="pill-button" onclick="changeContent('guide'); swapStyleSheet('meta/css/guide.css');">
+						<div id="bottom-pill-button" class="pill-button" onclick="changeContent('guide'); swapStyleSheet('meta/css/guide.css'); playAudio('decide');">
 							<u>Installation guide</u>
 						</div>
 					</div>
@@ -27,12 +27,12 @@ function changeContent(page) {
 		case 'guide':
 			contentDiv.innerHTML = `
 				<div class="navi-bar">
-					<h4 href="#top" onclick="changeContent('home'); swapStyleSheet('meta/css/main.css'); select;">Brewtendo</h4>
-					<h4 href="#top" onclick="changeContent('guide'); swapStyleSheet('meta/css/guide.css'); select;">Install Guide</h4>
-					<h4 href="#top" onclick="changeContent('badgearcade'); swapStyleSheet('meta/css/main.css'); select;">Badge Arcade</h4>
-					<h4 href="#top"onclick="changeContent('other'); swapStyleSheet('meta/css/main.css'); select;">Other</h4>
+					<h4 href="#top" onclick="changeContent('home'); swapStyleSheet('meta/css/main.css'); playAudio('decide');">Brewtendo</h4>
+					<h4 href="#top" onclick="changeContent('guide'); swapStyleSheet('meta/css/guide.css'); playAudio('decide');">Install Guide</h4>
+					<h4 href="#top" onclick="changeContent('badgearcade'); swapStyleSheet('meta/css/guide.css'); playAudio('decide');">Badge Arcade</h4>
+					<h4 href="#top"onclick="changeContent('other'); swapStyleSheet('meta/css/main.css'); playAudio('decide');">Other</h4>
 				</div>
-				<h1 id="header-guide">Brewtendo installation guide:</h1>
+				<h1 id="header-guide-orange">Brewtendo installation guide:</h1>
 				<div id="info-box">
 					<h1 class="guide-txt">You will need a modded 3DS with luma3ds installed see <a href="https://3ds.hacks.guide/">https://3ds.hacks.guide/</a> for more info.</h1>
 					<h1 class="guide-txt" class="header-column"><u>Setting up the Brewtendo eShop:</u></h1>
@@ -54,7 +54,7 @@ function changeContent(page) {
 				<hr class="hide">
 				<hr class="hide">
 				<div id="center-div">
-					<div class="pill-button" onclick="changeContent('home'); swapStyleSheet('meta/css/main.css');">
+					<div class="pill-button" onclick="changeContent('home'); swapStyleSheet('meta/css/main.css'); playAudio('decide');">
 						<u>Home Page</u>
 					</div>
 				</div>
@@ -66,21 +66,50 @@ function changeContent(page) {
 		case 'badgearcade':
 			contentDiv.innerHTML = `
 				<div class="navi-bar">
-					<h4 href="#top" onclick="changeContent('home'); swapStyleSheet('meta/css/main.css'); select;">Brewtendo</h4>
-					<h4 href="#top" onclick="changeContent('guide'); swapStyleSheet('meta/css/guide.css'); select;">Install Guide</h4>
-					<h4 href="#top" onclick="changeContent('badgearcade'); swapStyleSheet('meta/css/main.css'); select;">Badge Arcade</h4>
-					<h4 href="#top" onclick="changeContent('other'); swapStyleSheet('meta/css/main.css'); select;">Other</h4>
+					<h4 href="#top" onclick="changeContent('home'); swapStyleSheet('meta/css/main.css'); playAudio('decide');">Brewtendo</h4>
+					<h4 href="#top" onclick="changeContent('guide'); swapStyleSheet('meta/css/guide.css'); playAudio('decide');">Install Guide</h4>
+					<h4 href="#top" onclick="changeContent('badgearcade'); swapStyleSheet('meta/css/guide.css'); playAudio('decide');">Badge Arcade</h4>
+					<h4 href="#top" onclick="changeContent('other'); swapStyleSheet('meta/css/main.css'); playAudio('decide');">Other</h4>
 				</div>
-				<h1>nothing yet :)</h1>
+				<h1 id="header-guide-red">Badge Arcade Cheats:</h1>
+				<div id="info-box">
+					<h1 class="guide-txt" class="header-column"><u>Setup Instructions</u></h1>
+					<h1 class="guide-txt" class="header-column"><u>Install the Plugin:</u></h1>
+					<h1 class="guide-txt">Place the <a href="https://drive.google.com/uc?export=download&id=1wBAnV2fCdtEDNQJFrU6TwN_23XbRc6GU">cheat plugin</a> file into your SD card under <code>luma/plugins.</code></h1>
+					<h1 class="guide-txt">If the folder does not exist, create it manually.</h1>
+					<h1 class="guide-txt"><u>Enable Plugin Loader:</u></h1>
+					<h1 class="guide-txt">Launch Rosalina Menu: Hold L + D-Pad Down + Select & Navigate to Plugin Loader and enable it. then enter badge arcade.</h1>
+					<h1 class="guide-txt" class="header-column"><u>Cheat Procedure</u></h1>
+					<h1 class="guide-txt">Press Select (or L + D-Pad Down + Select) to enter the search interface.</h1>
+					<h1 class="guide-txt">Search for the value corresponding to your available plays (starting at 5 or the amount of plays you have).</h1>
+					<h1 class="guide-txt">Play until the remaining plays decrease to 4.</h1>
+					<h1 class="guide-txt">then search again but with 4 but search 3 once following previous step again if the search results appear with more than 4 in the list, continue playing until only 3 values remain.</h1>
+					<h1 class="guide-txt">Ensure at least 2–4 values are present in the search list.</h1>
+					<h1 class="guide-txt">Edit all selected values to 9 to maximize the number of plays.</h1>
+					<h1 class="guide-txt">When returning to the game, if no arm11, the cheat should automatically adjust remaining plays.</h1>
+					<h1 class="guide-txt">Instead of showing 3 plays left, it will display 8 plays (or the appropriate maximum).</h1>
+					<h1 class="guide-txt">The exact maximum value is unknown, but setting all target values to 9 ensures the cheat works reliably.</h1>
+				</div>
+				<hr class="hide">
+				<hr class="hide">
+				<hr class="hide">
+				<div id="center-div">
+					<div class="pill-button" onclick="changeContent('home'); swapStyleSheet('meta/css/main.css'); playAudio('decide');">
+						<u>Home Page</u>
+					</div>
+				</div>
+				<hr class="hide">
+				<hr class="hide">
+				<hr class="hide">
 			`;
 			break;
 		case 'other':
 			contentDiv.innerHTML = `
 				<div class="navi-bar">
-					<h4 href="#top" onclick="changeContent('home'); swapStyleSheet('meta/css/main.css'); select;">Brewtendo</h4>
-					<h4 href="#top" onclick="changeContent('guide'); swapStyleSheet('meta/css/guide.css'); select;">Install Guide</h4>
-					<h4 href="#top" onclick="changeContent('badgearcade'); swapStyleSheet('meta/css/main.css'); select;">Badge Arcade</h4>
-					<h4 href="#top" onclick="changeContent('other'); swapStyleSheet('meta/css/main.css'); select;">Other</h4>
+					<h4 href="#top" onclick="changeContent('home'); swapStyleSheet('meta/css/main.css'); playAudio('decide');">Brewtendo</h4>
+					<h4 href="#top" onclick="changeContent('guide'); swapStyleSheet('meta/css/guide.css'); playAudio('decide');">Install Guide</h4>
+					<h4 href="#top" onclick="changeContent('badgearcade'); swapStyleSheet('meta/css/guide.css'); playAudio('decide');">Badge Arcade</h4>
+					<h4 href="#top" onclick="changeContent('other'); swapStyleSheet('meta/css/main.css'); playAudio('decide');">Other</h4>
 				</div>
 				<h1>nothing yet :)</h1>
 			`;
