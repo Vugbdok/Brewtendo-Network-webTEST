128a01e7c94c31ad7ac53d10d0a3f08d9a1dad90
cannedfart <jayromeramirez19@gmail.com>
2026-03-19 08:32:23 -0400

    Update content_03.js

--- meta/js/content_03.js
+++ meta/js/content_03.js
@@ -4,24 +4,24 @@ function load_3() {
             <div id="center-navi">
                 <h4 href="#top" onclick="changeContent('home'); swapStyleSheet('meta/css/main.css'); playAudio('decide');">Brewtendo</h4>
                 <h4 href="#top" onclick="changeContent('guide'); swapStyleSheet('meta/css/guide.css'); playAudio('decide');">Install Guide</h4>
-                <h4 href="#top" onclick="changeContent('badgearcade'); swapStyleSheet('meta/css/guide.css'); playAudio('decide');"><u>Badge Arcade</u></h4>
+                <h4 href="#top" onclick="changeContent('badgearcade'); swapStyleSheet('meta/css/guide.css'); playAudio('decide');">Badge Arcade</h4>
                 <h4 href="#top" onclick="changeContent('other'); swapStyleSheet('meta/css/main.css'); playAudio('decide');" style="border: none;">Other Info</h4>
             </div>
         </div>
 			<h1 id="header-guide-red">Badge Arcade Cheats:</h1>
 		<div class="style-center-div">
-			<h1 class="guide-txt" class="header-column"><u>Setup Instructions</u></h1>
-			<h1 class="guide-txt" class="header-column"><u>Install the Plugin:</u></h1>
+			<h1 class="guide-txt" class="header-column">Setup Instructions</h1>
+			<h1 class="guide-txt" class="header-column">Install the Plugin:</h1>
 			<h1 class="guide-txt">Place the <a href="https://drive.google.com/uc?export=download&id=1wBAnV2fCdtEDNQJFrU6TwN_23XbRc6GU">cheat plugin</a> file into your SD card under <code>luma/plugins.</code></h1>
 			<h1 class="guide-txt">If the folder does not exist, create it manually.</h1>
-			<h1 class="guide-txt"><u>Enable Plugin Loader:</u></h1>
+			<h1 class="guide-txt">Enable Plugin Loader:</h1>
 			<h1 class="guide-txt">Launch Rosalina Menu: Hold L + D-Pad Down + Select & Navigate to Plugin Loader and enable it. then enter badge arcade.</h1>
-			<h1 class="guide-txt" class="header-column"><u>Cheat Procedure</u></h1>
+			<h1 class="guide-txt" class="header-column">Cheat Procedure</h1>
 			<h1 class="guide-txt">Press Select (or L + D-Pad Down + Select) to enter the search interface.</h1>
 			<h1 class="guide-txt">Search for the value corresponding to your available plays (starting at 5 or the amount of plays you have).</h1>
 			<h1 class="guide-txt">Play until the remaining plays decrease to 4.</h1>
 			<h1 class="guide-txt">then search again but with 4 but search 3 once following previous step again if the search results appear with more than 4 in the list, continue playing until only 3 values remain.</h1>
-			<h1 class="guide-txt">Ensure at least 2–4 values are present in the search list.</h1>
+			<h1 class="guide-txt">Ensure at least 2â€“4 values are present in the search list.</h1>
 			<h1 class="guide-txt">Edit all selected values to 9 to maximize the number of plays.</h1>
 			<h1 class="guide-txt">When returning to the game, if no arm11, the cheat should automatically adjust remaining plays.</h1>
 			<h1 class="guide-txt">Instead of showing 3 plays left, it will display 8 plays (or the appropriate maximum).</h1>
@@ -32,7 +32,7 @@ function load_3() {
 		<hr class="hide">
 		<div id="center-div">
 			<div class="pill-button" onclick="changeContent('home'); swapStyleSheet('meta/css/main.css'); playAudio('decide');">
-				<u>Home Page</u>
+				Home Page
 			</div>
 		</div>
 		<hr class="hide">
