27a08ee78cc03da18fd4108f6c5ca54434a83236
Vugbdok <164047914+Vugbdok@users.noreply.github.com>
2026-03-24 17:29:16 -0400

    Update content_02.js

--- old-site/meta/js/content_02.js
+++ old-site/meta/js/content_02.js
@@ -1,12 +1,12 @@
 function load_2() {
     return `
 		<div class="navi-bar">
-             <div id="center-navi">
-                   <h4 href="#top" onclick="changeContent('home'); swapStyleSheet('old-site/meta/css/main.css'); playAudio('decide');">Brewtendo</h4>
-                   <h4 href="#top" onclick="changeContent('guide'); swapStyleSheet('old-site/meta/css/guide.css'); playAudio('decide');">Install Guide</h4>
-                  <h4 href="#top" onclick="changeContent('badgearcade'); swapStyleSheet('old-site/meta/css/guide.css'); playAudio('decide');">Badge Arcade</h4>
-                  <h4 href="#top" onclick="changeContent('other'); swapStyleSheet('old-site/meta/css/main.css'); playAudio('decide');" style="border: none;">Other Info</h4>
-             </div>
+            <div id="center-navi">
+                <h4 href="#top" onclick="changeContent('home'); swapStyleSheet('old-site/meta/css/main.css'); playAudio('decide');">Brewtendo</h4>
+            	<h4 href="#top" onclick="changeContent('guide'); swapStyleSheet('old-site/meta/css/guide.css'); playAudio('decide');">Install Guide</h4>
+                <h4 href="#top" onclick="changeContent('badgearcade'); swapStyleSheet('old-site/meta/css/guide.css'); playAudio('decide');">Badge Arcade</h4>
+                <h4 href="#top" onclick="changeContent('other'); swapStyleSheet('old-site/meta/css/main.css'); playAudio('decide');" style="border: none;">Other Info</h4>
+            </div>
     	  </div>
 			<h1 id="header-guide-orange">Brewtendo installation guide:</h1>
 		            <div class="style-center-container" class="guide-txt">
