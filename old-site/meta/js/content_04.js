5ee28b732d02c27a7de83c2fd1f992f7e1fcf20c
Vugbdok <164047914+Vugbdok@users.noreply.github.com>
2026-03-24 20:20:38 -0400

    Adding Miis to content_04.js

--- old-site/meta/js/content_04.js
+++ old-site/meta/js/content_04.js
@@ -8,7 +8,9 @@ function load_04() {
                 <h4 href="#top" onclick="changeContent('other'); swapStyleSheet('old-site/meta/css/main.css'); playAudio('decide');" style="border: none;">Other Info</h4>
             </div>
     	</div>
-			<img src="https://brewtendonetwork.github.io/assets/3DS_Icon_Brew-1.png" width="200" alt="Brewtendo"/>
+			<img src="old-site/meta/miis/Adrian_render_headshot_2026-03-24T22_43_30.373Z.png" width="200" alt="Brewtendo"/>
+			<img src="old-site/meta/miis/Adrian_render_headshot_2026-03-24T22_43_30.373Z.png" width="200" alt="Brewtendo"/>
+			<img src="old-site/meta/miis/Adrian_render_headshot_2026-03-24T22_43_30.373Z.png" width="200" alt="Brewtendo"/>
 		</div>
 		</div>
     `;
