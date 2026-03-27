a0c69d7a5843d6cf32931047c4e421efb6d645a9
Vugbdok <adrian.bautista0107@gmail.com>
2026-03-27 10:28:44 -0400

    Update content_04.js

--- old-site/meta/js/content_04.js
+++ old-site/meta/js/content_04.js
@@ -10,7 +10,10 @@ function load_04() {
     	</div>
 			<img src="old-site/meta/images/MiiTeamData/FounderTeam.png" width="250" alt="Founder"/>
 			<img src="old-site/meta/images/MiiTeamData/OwnerTeam.png" width="200" alt="Owner"/>
-			<img src="old-site/meta/images/MiiTeamData/AdminTeam.png" width="800" alt="Administrators"/>
+			<img src="old-site/meta/images/MiiTeamData/DevTeam.png" width="800" alt="Administrators"/>
+			<img src="old-site/meta/images/MiiTeamData/UITeam.png" width="800" alt="Administrators"/>
+			<img src="old-site/meta/images/MiiTeamData/ShopmanTeam.png" width="800" alt="Administrators"/>
+			<img src="old-site/meta/images/MiiTeamData/ModTeam.png" width="800" alt="Administrators"/>
 		</div>
 		</div>
     `;
